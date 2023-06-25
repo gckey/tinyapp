5135762f89ace38b6e98977bb2ee4cb4478c7339
Abdurahman <abdorehman@gmail.com>
2023-06-24 23:40:35 -0600

    Add an additional route within express_server.js to render a new template

--- express_server.js
+++ express_server.js
@@ -17,14 +17,20 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get("/urls", (req, res) => {
   //to keep track of all the URLs and their shortened forms
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars); //to pass the url data to template
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  //retrieve the value, create an obj with template variables
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
