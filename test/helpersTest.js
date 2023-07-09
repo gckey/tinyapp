5f4a329c58849edb3af2d70273731fd4788d2575
Abdurahman <abdorehman@gmail.com>
2023-07-09 12:24:54 -0600

    Implement helpersTest with Mocha and Chai

---
+++ test/helpersTest.js
@@ -0,0 +1,45 @@
+const { assert } = require("chai");
+
+const { getUserByEmail } = require("../helpers.js");
+
+const testUsers = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = testUsers.userRandomID;
+    // Write your assert statement here
+    console.log(user);
+    assert.deepEqual(user, expectedUserID);
+  });
+
+  it("should return a user object with a valid email", function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedOutput = {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur",
+    };
+    console.log(user);
+    assert.deepEqual(user, expectedOutput);
+  });
+
+  it("should return undefined when passed invalid email", function() {
+    const user = getUserByEmail("sobrock@hello.com", testUsers);
+    const expectedOutput = undefined;
+
+    console.log(user);
+    assert.equal(user, expectedOutput);
+  });
+});
